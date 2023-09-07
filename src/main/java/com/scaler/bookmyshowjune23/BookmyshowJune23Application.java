8d242047748743d0b81c2bc05eaa6533daad4ab2
deepakkasera <deepak.kasera143.dk@gmail.com>
2023-09-07 09:24:27 +0530

    BMS Code 1

---
+++ src/main/java/com/scaler/bookmyshowjune23/BookmyshowJune23Application.java
@@ -0,0 +1,15 @@
+package com.scaler.bookmyshowjune23;
+
+import models.BaseModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowJune23Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookmyshowJune23Application.class, args);
+    }
+
+}
---
+++ src/main/java/models/Booking.java
@@ -0,0 +1,19 @@
+package models;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+@Getter
+@Setter
+@Entity
+public class Booking extends BaseModel { //Ticket.
+    private User user;
+    private List<ShowSeat> showSeats;
+    private int amount;
+    private BookingStatus bookingStatus;
+    private Date bookedDate;
+    private List<Payment> payments;
+}
